0726e4d1bde92d9d32b7bd3cf75cbb198ea1a9c0
Mathis <mathis.vinay@troizaire.com>
2021-07-15 12:13:16 +0200

    [-] fixed incorrectly written tests

--- unittest/test_template_scan.py
+++ unittest/test_template_scan.py
@@ -7,38 +7,38 @@ connexion = ootemplate.Connexion("localhost", "2002")
 class Text(unittest.TestCase):
 
     def test_noformat(self):
-        self.assertEqual(ootemplate.Template("unittest/files/text_vars_noformat.odt", connexion, False).scan(),
-                         {"gjerg": None, "jean": None, "aerhh": None, "rh": None, "aet": None, "h": None})
+        self.assertEqual({"gjerg": None, "jean": None, "aerhh": None, "rh": None, "aet": None, "h": None},
+                         ootemplate.Template("unittest/files/text_vars_noformat.odt", connexion, False).scan())
 
     def test_format(self):
-        self.assertEqual(ootemplate.Template("unittest/files/text_vars.odt", connexion, False).scan(),
-                         {"gjerg": None, "jean": None, "aerhh": None, "rh": None, "aet": None, "h": None})
+        self.assertEqual({"gjerg": None, "jean": None, "aerhh": None, "rh": None, "aet": None, "h": None},
+                         ootemplate.Template("unittest/files/text_vars.odt", connexion, False).scan())
 
     def test_static_table(self):
-        self.assertEqual(ootemplate.Template("unittest/files/static_tab.odt", connexion, False).scan(),
-                         {"var1": None, "var2": None})
+        self.assertEqual({"var1": None, "var2": None},
+                         ootemplate.Template("unittest/files/static_tab.odt", connexion, False).scan())
 
 
-class Img(unittest.TestCase):
+class Images(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(ootemplate.Template("unittest/files/img_vars.odt", connexion, False).scan(),
-                         {"image": {"path": None}})
+        self.assertEqual({"image": {"path": None}},
+                         ootemplate.Template("unittest/files/img_vars.odt", connexion, False).scan())
 
 
-class Table(unittest.TestCase):
+class Tables(unittest.TestCase):
 
     def test_multiple_row(self):
-        self.assertEqual(ootemplate.Template("unittest/files/multiple_row_tab.odt", connexion, False).scan(),
-                         {"tab": [{"var1": None, "var2": None}]})
+        self.assertEqual({"tab": [{"var1": None, "var2": None}]},
+                         ootemplate.Template("unittest/files/multiple_row_tab.odt", connexion, False).scan())
 
     def test_one_row_varied(self):
-        self.assertEqual(ootemplate.Template("unittest/files/multiple_row_tab.odt", connexion, False).scan(),
-                         {"tab": [{"var": None}]})
+        self.assertEqual({"tab": [{"var": None}]},
+                         ootemplate.Template("unittest/files/one_row_tab_varied.odt", connexion, False).scan())
 
     def test_two_row_varied(self):
-        self.assertEqual(ootemplate.Template("unittest/files/two_row_tab_varied.odt", connexion, False).scan(),
-                         {"tab": [{"var1": None}]})
+        self.assertEqual({"tab": [{"var1": None}]},
+                         ootemplate.Template("unittest/files/two_row_tab_varied.odt", connexion, False).scan())
 
     def test_invalid_var(self):
         with self.assertRaises(ootemplate.TemplateVariableNotInLastRow):
@@ -49,8 +49,8 @@ class Table(unittest.TestCase):
             ootemplate.Template("unittest/files/invalid_vars_tab.odt", connexion, False).scan()
 
     def test_two_tabs_varied(self):
-        self.assertEqual(ootemplate.Template("unittest/files/two_tabs_varied.odt", connexion, False).scan(),
-                         {"tab": [{"var1": None}], "tab2": [{"1": None, "2": None, "3": None, "4": None, "5": None}]})
+        self.assertEqual({"tab": [{"var1": None}], "tab2": [{"1": None, "2": None, "3": None, "4": None, "5": None}]},
+                         ootemplate.Template("unittest/files/two_tabs_varied.odt", connexion, False).scan())
 
 
 if __name__ == '__main__':
