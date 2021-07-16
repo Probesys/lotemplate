ce932ccb147f29d9b689f28c64021a37fec9522f
Mathis <mathis.vinay@troizaire.com>
2021-07-16 16:39:28 +0200

    [+] added tables unitary tests

--- unittest/test_comparaison.py
+++ unittest/test_comparaison.py
@@ -25,6 +25,37 @@ class Text(unittest.TestCase):
         with self.assertRaises(ootemplate.err.JsonMissingRequiredVariable):
             self.temp.compare_variables(to_data_list("files/comparaison/text_vars_invalid_missing_variable.json"))
 
+    def test_invalid_incorrect_value(self):
+        with self.assertRaises(ootemplate.err.JsonIncorrectValueType):
+            self.temp.compare_variables(to_data_list("files/comparaison/text_vars_incorrect_value.json"))
+
+    temp_tab = ootemplate.Template("unittest/files/comparaison/static_tab.odt", cnx, False)
+
+    def test_tab_valid(self):
+        datas = to_data_list("files/comparaison/static_tab_valid.json")
+        self.assertEqual(datas, self.temp.compare_variables(datas))
+
+
+class Tables(unittest.TestCase):
+
+    temp = ootemplate.Template("unittest/files/comparaison/two_row_tab_varied.odt", cnx, False)
+
+    def test_valid(self):
+        datas = to_data_list("files/comparaison/two_row_tab_varied_valid.json")
+        self.assertEqual(datas, self.temp.compare_variables(datas))
+
+    def test_invalid_missing_variable(self):
+        with self.assertRaises(ootemplate.err.JsonMissingRequiredVariable):
+            self.temp.compare_variables(to_data_list(
+                "files/comparaison/two_row_tab_varied_invalid_missing_argument_all_rows.json"
+            ))
+
+    def test_invalid_unknown_variable(self):
+        with self.assertRaises(ootemplate.err.JsonUnknownVariable):
+            self.temp.compare_variables(to_data_list(
+                "files/comparaison/two_row_tab_varied_invalid_unknown_argument.json"
+            ))
+
 
 if __name__ == '__main__':
     unittest.main()
