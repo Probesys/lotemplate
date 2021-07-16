f65158e591cae36616cf7e91f5c6f91cab099a19
Mathis <mathis.vinay@troizaire.com>
2021-07-16 14:54:30 +0200

    [+] added unitary tests for json convert with tables

--- unittest/test_json_convertion.py
+++ unittest/test_json_convertion.py
@@ -43,42 +43,96 @@ class Generic(unittest.TestCase):
 class Text(unittest.TestCase):
 
     def test_vars_valid(self):
-        self.assertEqual({"aerhh": "", "h": "", "rh": "", "gjerg": "", "aet": "", "jean": ""},
-                         ootemplate.convert_to_datas_template(
-                             "text_vars_valid.json", file_to_dict("files/jsons/text_vars_valid.json")))
+        self.assertEqual(
+            {"aerhh": "", "h": "", "rh": "", "gjerg": "", "aet": "", "jean": ""},
+            ootemplate.convert_to_datas_template(
+                "text_vars_valid.json", file_to_dict("files/jsons/text_vars_valid.json")
+            )
+        )
 
 
 class Images(unittest.TestCase):
 
     def test_valid(self):
-        self.assertEqual({"image": {"path": ""}},
-                         ootemplate.convert_to_datas_template(
-                             "img_vars_valid", file_to_dict("files/jsons/img_vars_valid.json")))
+        self.assertEqual(
+            {"image": {"path": ""}}, ootemplate.convert_to_datas_template(
+                "img_vars_valid", file_to_dict("files/jsons/img_vars_valid.json"))
+        )
 
     def test_two_valid(self):
-        self.assertEqual({"image": {"path": ""}, "image2": {"path": ""}},
-                         ootemplate.convert_to_datas_template(
-                             "img_two_vars_valid", file_to_dict("files/jsons/img_two_vars_valid.json")))
+        self.assertEqual(
+            {"image": {"path": ""}, "image2": {"path": ""}}, ootemplate.convert_to_datas_template(
+                "img_two_vars_valid", file_to_dict("files/jsons/img_two_vars_valid.json"))
+        )
 
     def test_invalid_empty_value(self):
         with self.assertRaises(ootemplate.err.JsonEmptyValue):
             ootemplate.convert_to_datas_template(
-                "img_vars_invalid_empty_value.json", file_to_dict("files/jsons/img_vars_invalid_empty_value.json"))
+                "img_vars_invalid_empty_value.json", file_to_dict("files/jsons/img_vars_invalid_empty_value.json")
+            )
 
     def test_invalid_null_path(self):
         with self.assertRaises(ootemplate.err.JsonInvalidArgument):
             ootemplate.convert_to_datas_template(
-                "img_vars_invalid_null_path_type", file_to_dict("files/jsons/img_vars_invalid_null_path_type.json"))
+                "img_vars_invalid_null_path_type", file_to_dict("files/jsons/img_vars_invalid_null_path_type.json")
+            )
 
     def test_invalid_other_argument(self):
         with self.assertRaises(ootemplate.err.JsonUnknownArgument):
             ootemplate.convert_to_datas_template(
-                "img_vars_invalid_other_argument", file_to_dict("files/jsons/img_vars_invalid_other_argument.json"))
+                "img_vars_invalid_other_argument", file_to_dict("files/jsons/img_vars_invalid_other_argument.json")
+            )
 
     def test_invalid_path_type(self):
         with self.assertRaises(ootemplate.err.JsonInvalidArgument):
             ootemplate.convert_to_datas_template(
-                "img_vars_invalid_path_type", file_to_dict("files/jsons/img_vars_invalid_path_type.json"))
+                "img_vars_invalid_path_type", file_to_dict("files/jsons/img_vars_invalid_path_type.json")
+            )
+
+
+class Tables(unittest.TestCase):
+
+    def test_valid(self):
+        self.assertEqual(
+            {"tab": [{"var": ""}], "tab2": [{"var1": "", "var2": ""}]},
+            ootemplate.convert_to_datas_template("tab_valid", file_to_dict("files/jsons/tab_valid.json"))
+        )
+
+    def test_invalid_empty_table(self):
+        with self.assertRaises(ootemplate.err.JsonEmptyValue):
+            ootemplate.convert_to_datas_template(
+                "tab_invalid_empty_table", file_to_dict("files/jsons/tab_invalid_empty_table.json")
+            )
+
+    def test_invalid_empty_row(self):
+        with self.assertRaises(ootemplate.err.JsonEmptyValue):
+            ootemplate.convert_to_datas_template(
+                "tab_invalid_empty_row", file_to_dict("files/jsons/tab_invalid_empty_row.json")
+            )
+
+    def test_invalid_row_value(self):
+        with self.assertRaises(ootemplate.err.JsonIncorrectValueType):
+            ootemplate.convert_to_datas_template(
+                "tab_invalid_value_type_row", file_to_dict("files/jsons/tab_invalid_value_type_row.json")
+            )
+
+    def test_invalid_cell_value(self):
+        with self.assertRaises(ootemplate.err.JsonIncorrectValueType):
+            ootemplate.convert_to_datas_template(
+                "tab_invalid_value_type_cell", file_to_dict("files/jsons/tab_invalid_value_type_cell.json")
+            )
+
+    def test_invalid_missing_argument(self):
+        with self.assertRaises(ootemplate.err.JsonIncorrectTabVariables):
+            ootemplate.convert_to_datas_template(
+                "tab_invalid_missing_argument", file_to_dict("files/jsons/tab_invalid_missing_argument.json")
+            )
+
+    def test_invalid_missing_argument2(self):
+        with self.assertRaises(ootemplate.err.JsonIncorrectTabVariables):
+            ootemplate.convert_to_datas_template(
+                "tab_invalid_missing_argument2", file_to_dict("files/jsons/tab_invalid_missing_argument2.json")
+            )
 
 
 if __name__ == '__main__':
