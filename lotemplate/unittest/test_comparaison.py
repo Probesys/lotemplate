680347d17e592fd42efff887d1e35f296b7fa350
mathis <mathis.vinay@troizaire.com>
2023-01-05 12:05:48 +0100

    🧪 working dir modification

--- lotemplate/unittest/test_comparaison.py
+++ lotemplate/unittest/test_comparaison.py
@@ -15,63 +15,64 @@ def to_data(file: str):
 
 class Text(unittest.TestCase):
 
-    temp = ot.Template("files/comparaison/text_vars.odt", cnx, True)
+    temp = ot.Template("lotemplate/unittest/files/comparaison/text_vars.odt", cnx, True)
 
     def test_valid(self):
-        self.temp.search_error(to_data("files/comparaison/text_vars_valid.json"))
+        self.temp.search_error(to_data("lotemplate/unittest/files/comparaison/text_vars_valid.json"))
 
     def test_invalid_supplement_variable(self):
         with self.assertRaises(ot.errors.JsonComparaisonError):
-            self.temp.search_error(to_data("files/comparaison/text_vars_invalid_variable.json"))
+            self.temp.search_error(to_data("lotemplate/unittest/files/comparaison/text_vars_invalid_variable.json"))
 
     def test_invalid_missing_variable(self):
         with self.assertRaises(ot.errors.JsonComparaisonError):
-            self.temp.search_error(to_data("files/comparaison/text_vars_invalid_missing_variable.json"))
+            self.temp.search_error(
+                to_data("lotemplate/unittest/files/comparaison/text_vars_invalid_missing_variable.json"))
 
     def test_invalid_incorrect_value(self):
         with self.assertRaises(ot.errors.JsonComparaisonError):
-            self.temp.search_error(to_data("files/comparaison/text_vars_incorrect_value.json"))
+            self.temp.search_error(to_data("lotemplate/unittest/files/comparaison/text_vars_incorrect_value.json"))
 
     temp.close()
 
-    temp_tab = ot.Template("files/comparaison/static_tab.odt", cnx, True)
+    temp_tab = ot.Template("lotemplate/unittest/files/comparaison/static_tab.odt", cnx, True)
 
     def test_tab_valid(self):
-        self.temp_tab.search_error(to_data("files/comparaison/static_tab_valid.json"))
+        self.temp_tab.search_error(to_data("lotemplate/unittest/files/comparaison/static_tab_valid.json"))
 
     temp_tab.close()
 
 
 class Tables(unittest.TestCase):
 
-    temp = ot.Template("files/comparaison/two_row_tab_varied.odt", cnx, True)
+    temp = ot.Template("lotemplate/unittest/files/comparaison/two_row_tab_varied.odt", cnx, True)
 
     def test_valid(self):
-        self.temp.search_error(to_data("files/comparaison/two_row_tab_varied_valid.json"))
+        self.temp.search_error(to_data("lotemplate/unittest/files/comparaison/two_row_tab_varied_valid.json"))
 
     def test_invalid_missing_variable(self):
         with self.assertRaises(ot.errors.JsonComparaisonError):
-            self.temp.search_error(
-                to_data("files/comparaison/two_row_tab_varied_invalid_missing_argument_all_rows.json")
-            )
+            self.temp.search_error(to_data(
+                "lotemplate/unittest/files/comparaison/two_row_tab_varied_invalid_missing_argument_all_rows.json"))
 
     def test_invalid_unknown_variable(self):
         with self.assertRaises(ot.errors.JsonComparaisonError):
-            self.temp.search_error(to_data("files/comparaison/two_row_tab_varied_invalid_unknown_argument.json"))
+            self.temp.search_error(to_data(
+                "lotemplate/unittest/files/comparaison/two_row_tab_varied_invalid_unknown_argument.json"))
 
     temp.close()
 
 
 class Images(unittest.TestCase):
 
-    temp = ot.Template("files/comparaison/img_vars.odt", cnx, True)
+    temp = ot.Template("lotemplate/unittest/files/comparaison/img_vars.odt", cnx, True)
 
     def test_valid(self):
-        self.temp.search_error(to_data("files/comparaison/img_vars_valid.json"))
+        self.temp.search_error(to_data("lotemplate/unittest/files/comparaison/img_vars_valid.json"))
 
     def test_invalid_unknown_variable(self):
         with self.assertRaises(ot.errors.JsonComparaisonError):
-            self.temp.search_error(to_data("files/comparaison/img_vars_invalid_other_image.json"))
+            self.temp.search_error(to_data("lotemplate/unittest/files/comparaison/img_vars_invalid_other_image.json"))
 
     temp.close()
 
--- lotemplate/unittest/test_template_scan.py
+++ lotemplate/unittest/test_template_scan.py
@@ -12,29 +12,41 @@ class Text(unittest.TestCase):
 
     def test_noformat(self):
         self.assertEqual(
-            {"gjerg": {"type": "text", "value": ""}, "jean": {"type": "text", "value": ""}, "aerhh": {"type": "text", "value": ""}, "rh": {"type": "text", "value": ""}, "aet": {"type": "text", "value": ""}, "h": {"type": "text", "value": ""}},
-            (doc := ot.Template("files/templates/text_vars_noformat.odt", cnx, False)).scan()
-        )
+            {
+                "gjerg": {"type": "text", "value": ""},
+                "jean": {"type": "text", "value": ""},
+                "aerhh": {"type": "text", "value": ""},
+                "rh": {"type": "text", "value": ""},
+                "aet": {"type": "text", "value": ""},
+                "h": {"type": "text", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/text_vars_noformat.odt", cnx, False)).scan())
         doc.close()
 
     def test_format(self):
         self.assertEqual(
-            {"gjerg": {"type": "text", "value": ""}, "jean": {"type": "text", "value": ""}, "aerhh": {"type": "text", "value": ""}, "rh": {"type": "text", "value": ""}, "aet": {"type": "text", "value": ""}, "h": {"type": "text", "value": ""}},
-            (doc := ot.Template("files/templates/text_vars.odt", cnx, False)).scan()
-        )
+            {
+                "gjerg": {"type": "text", "value": ""},
+                "jean": {"type": "text", "value": ""},
+                "aerhh": {"type": "text", "value": ""},
+                "rh": {"type": "text", "value": ""},
+                "aet": {"type": "text", "value": ""},
+                "h": {"type": "text", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/text_vars.odt", cnx, False)).scan())
         doc.close()
 
     def test_static_table(self):
         self.assertEqual(
             {"var1": {"type": "text", "value": ""}, "var2": {"type": "text", "value": ""}},
-            (doc := ot.Template("files/templates/static_tab.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/static_tab.odt", cnx, False)).scan()
         )
         doc.close()
 
     def test_function_variable(self):
         self.assertEqual(
             {"test(\"jean\")": {"type": "text", "value": ""}},
-            (doc := ot.Template("files/templates/function_variable.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/function_variable.odt", cnx, False)).scan()
         )
         doc.close()
 
@@ -44,14 +56,18 @@ class Images(unittest.TestCase):
     def test_one_image(self):
         self.assertEqual(
             {"image": {"type": "image", "value": ""}},
-            (doc := ot.Template("files/templates/img_vars.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/img_vars.odt", cnx, False)).scan()
         )
         doc.close()
 
     def test_multiple_images(self):
         self.assertEqual(
-            {"image1": {"type": "image", "value": ""}, "image2": {"type": "image", "value": ""}, "image3": {"type": "image", "value": ""}},
-            (doc := ot.Template("files/templates/multiple_img_vars.odt", cnx, False)).scan()
+            {
+                "image1": {"type": "image", "value": ""},
+                "image2": {"type": "image", "value": ""},
+                "image3": {"type": "image", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/multiple_img_vars.odt", cnx, False)).scan()
         )
         doc.close()
 
@@ -61,38 +77,45 @@ class Tables(unittest.TestCase):
     def test_multiple_row(self):
         self.assertEqual(
             {"var1": {"type": "table", "value": [""]}, "var2": {"type": "table", "value": [""]}},
-            (doc := ot.Template("files/templates/multiple_row_tab.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/multiple_row_tab.odt", cnx, False)).scan()
         )
         doc.close()
 
     def test_one_row_varied(self):
         self.assertEqual(
             {"var": {"type": "table", "value": [""]}},
-            (doc := ot.Template("files/templates/one_row_tab_varied.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/one_row_tab_varied.odt", cnx, False)).scan()
         )
         doc.close()
 
     def test_two_row_varied(self):
         self.assertEqual(
             {"var1": {"type": "table", "value": [""]}},
-            (doc := ot.Template("files/templates/two_row_tab_varied.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/two_row_tab_varied.odt", cnx, False)).scan()
         )
         doc.close()
 
     def test_invalid_var(self):
         with self.assertRaises(ot.errors.TemplateError):
-            (doc := ot.Template("files/templates/invalid_var_tab.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/invalid_var_tab.odt", cnx, False)).scan()
         doc.close()
 
     def test_invalid_vars(self):
         with self.assertRaises(ot.errors.TemplateError):
-            (doc := ot.Template("files/templates/invalid_vars_tab.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/invalid_vars_tab.odt", cnx, False)).scan()
         doc.close()
 
     def test_two_tabs_varied(self):
         self.assertEqual(
-            {"var1": {"type": "table", "value": [""]}, "1": {"type": "table", "value": [""]}, "2": {"type": "table", "value": [""]}, "3": {"type": "table", "value": [""]}, "4": {"type": "table", "value": [""]}, "5": {"type": "table", "value": [""]}},
-            (doc := ot.Template("files/templates/two_tabs_varied.odt", cnx, False)).scan()
+            {
+                "var1": {"type": "table", "value": [""]},
+                "1": {"type": "table", "value": [""]},
+                "2": {"type": "table", "value": [""]},
+                "3": {"type": "table", "value": [""]},
+                "4": {"type": "table", "value": [""]},
+                "5": {"type": "table", "value": [""]}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/two_tabs_varied.odt", cnx, False)).scan()
         )
         doc.close()
 
@@ -114,7 +137,7 @@ class Generic(unittest.TestCase):
                 "static2": {"type": "text", "value": ""},
                 "static3": {"type": "text", "value": ""}
             },
-            (doc := ot.Template("files/templates/multiple_variables.odt", cnx, False)).scan())
+            (doc := ot.Template("lotemplate/unittest/files/templates/multiple_variables.odt", cnx, False)).scan())
         doc.close()
 
     def test_multiple_pages(self):
@@ -134,14 +157,15 @@ class Generic(unittest.TestCase):
                 "date": {"type": "text", "value": ""},
                 "lieu": {"type": "text", "value": ""}
             },
-            (doc := ot.Template("files/templates/multiple_pages.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/multiple_pages.odt", cnx, False)).scan()
         )
         doc.close()
 
     def test_online_empty_doc(self):
         self.assertEqual(
             {},
-            (doc := ot.Template("https://www.mtsac.edu/webdesign/accessible-docs/word/example03.docx", cnx, False)).scan()
+            (doc := ot.Template(
+                "https://www.mtsac.edu/webdesign/accessible-docs/word/example03.docx", cnx, False)).scan()
         )
         doc.close()
 
@@ -151,7 +175,7 @@ class Generic(unittest.TestCase):
 
     def test_duplicated_variable(self):
         with self.assertRaises(ot.errors.TemplateError):
-            (doc := ot.Template("files/templates/duplicated_variables.odt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/duplicated_variables.odt", cnx, False)).scan()
         doc.close()
 
 
@@ -159,42 +183,69 @@ class OtherFormats(unittest.TestCase):
 
     def test_ott(self):
         self.assertEqual(
-            {"cell1": {"type": "table", "value": [""]}, "cell2": {"type": "table", "value": [""]}, "Nom": {"type": "text", "value": ""}, "prenon": {"type": "text", "value": ""}, "signature": {"type": "text", "value": ""}, "photo": {"type": "image", "value": ""}},
-            (doc := ot.Template("files/templates/format.ott", cnx, False)).scan()
+            {
+                "cell1": {"type": "table", "value": [""]},
+                "cell2": {"type": "table", "value": [""]},
+                "Nom": {"type": "text", "value": ""},
+                "prenon": {"type": "text", "value": ""},
+                "signature": {"type": "text", "value": ""},
+                "photo": {"type": "image", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/format.ott", cnx, False)).scan()
         )
         doc.close()
 
     def test_docx(self):
         self.assertEqual(
-            {"cell1": {"type": "text", "value": ""}, "cell2": {"type": "text", "value": ""}, "Nom": {"type": "text", "value": ""}, "prenon": {"type": "text", "value": ""}, "signature": {"type": "text", "value": ""}, "photo": {"type": "image", "value": ""}},
-            (doc := ot.Template("files/templates/format.docx", cnx, False)).scan()
+            {
+                "cell1": {"type": "text", "value": ""},
+                "cell2": {"type": "text", "value": ""},
+                "Nom": {"type": "text", "value": ""},
+                "prenon": {"type": "text", "value": ""},
+                "signature": {"type": "text", "value": ""},
+                "photo": {"type": "image", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/format.docx", cnx, False)).scan()
         )
         doc.close()
 
     def test_text(self):
         self.assertEqual(
             {"signature": {"type": "text", "value": ""}},
-            (doc := ot.Template("files/templates/format.txt", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/format.txt", cnx, False)).scan()
         )
         doc.close()
 
     def test_html(self):
         self.assertEqual(
-            {"cell1": {"type": "text", "value": ""}, "cell2": {"type": "text", "value": ""}, "Nom": {"type": "text", "value": ""}, "prenon": {"type": "text", "value": ""}, "signature": {"type": "text", "value": ""}, "photo": {"type": "image", "value": ""}},
-            (doc := ot.Template("files/templates/format.html", cnx, False)).scan()
+            {
+                "cell1": {"type": "text", "value": ""},
+                "cell2": {"type": "text", "value": ""},
+                "Nom": {"type": "text", "value": ""},
+                "prenon": {"type": "text", "value": ""},
+                "signature": {"type": "text", "value": ""},
+                "photo": {"type": "image", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/format.html", cnx, False)).scan()
         )
         doc.close()
 
     def test_rtf(self):
         self.assertEqual(
-            {"cell1": {"type": "text", "value": ""}, "cell2": {"type": "text", "value": ""}, "Nom": {"type": "text", "value": ""}, "prenon": {"type": "text", "value": ""}, "signature": {"type": "text", "value": ""}},
-            (doc := ot.Template("files/templates/format.rtf", cnx, False)).scan()
+            {
+                "cell1": {"type": "text", "value": ""},
+                "cell2": {"type": "text", "value": ""},
+                "Nom": {"type": "text", "value": ""},
+                "prenon": {"type": "text", "value": ""},
+                "signature": {"type": "text", "value": ""}
+            },
+            (doc := ot.Template("lotemplate/unittest/files/templates/format.rtf", cnx, False)).scan()
         )
         doc.close()
 
     def test_invalid(self):
         with self.assertRaises(ot.errors.TemplateError):
-            (doc := ot.Template("files/templates/invalid_format.jpg", cnx, False)).scan()
+            (doc := ot.Template("lotemplate/unittest/files/templates/invalid_format.jpg", cnx, False)).scan()
             doc.close()
 
 
